903a67466a05023cf7f0b829eec193290806353e
Alexander Krasilnikov <i@krasilnikov-1.ru>
2023-03-26 19:53:30 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public FormDate birthday;
+    public boolean subscription;
+
+}
+
+
